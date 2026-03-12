b1bf7834f462eb28c14bdb7fd33d78bc0bb41df8
Marlon4789 <cuartasm844@gmail.com>
2026-03-11 20:56:52 -0500

    fix: add default redis url for celery

--- core/settings.py
+++ core/settings.py
@@ -235,8 +235,8 @@ stripe.api_key = STRIPE_SECRET_KEY
 # CELERY CONFIGURATION
 # =========================================================
 
-CELERY_BROKER_URL = config("REDIS_URL")
-CELERY_RESULT_BACKEND = config("REDIS_URL")
+CELERY_BROKER_URL = config("REDIS_URL", default="redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = config("REDIS_URL", default="redis://localhost:6379/0")
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
