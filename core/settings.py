f5c2df7c20a786647d373f284af6df10e6a76a23
Marlon4789 <cuartasm844@gmail.com>
2026-03-16 15:53:01 -0500

    fix: sendgrid with yarmdev email, no hardcoded values

--- core/settings.py
+++ core/settings.py
@@ -213,19 +213,13 @@ CART_SESSION_ID = "cart"
 
 
 # =========================================================
-# EMAIL CONFIGURATION (GMAIL SMTP)
+# EMAIL CONFIGURATION (SENDGRID)
 # =========================================================
+SENDGRID_API_KEY = config('SENDGRID_API_KEY', default='')
+EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 # =========================================================
