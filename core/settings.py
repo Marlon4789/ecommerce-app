248d2ae465a42277f7eb2da188a3dcc4b3f8eb08
Marlon4789 <cuartasm844@gmail.com>
2026-03-17 10:16:27 -0500

    fix: configure email backend and verify email sending with console backend

--- core/settings.py
+++ core/settings.py
@@ -217,9 +217,9 @@ CART_SESSION_ID = "cart"
 # =========================================================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=config('EMAIL_HOST_USER'))
